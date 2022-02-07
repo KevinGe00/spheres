1eae29db5b6dcbf8d9f3101842fc5711e804b7a7
Ng Bob Shoaun <ngbobshoaun2000@gmail.com>
2022-02-06 19:51:43 -0500

    update index.jsx stules

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,18 @@
-import { ethers } from 'ethers'
-import Link from 'next/link'
-import axios from 'axios'
-import styles from '../styles/Home.module.css'
-import Image from 'next/image'
-import logo from './Images/Landing/logo.png';
-import shpereText from './Images/Landing/Spheres.png';
-import text from './Images/Landing/Play. Connect. Explore..png';
-import connectWalletButton from './Images/Landing/connectWallet.png';
+import { ethers } from "ethers";
+import Link from "next/link";
+import axios from "axios";
+import styles from "../styles/Home.module.css";
+import Image from "next/image";
+import logo from "./Images/Landing/logo.png";
+import shpereText from "./Images/Landing/Spheres.png";
+import text from "./Images/Landing/Play. Connect. Explore..png";
+import connectWalletButton from "./Images/Landing/connectWallet.png";
 
-import { useGlobalState, setCurrentAccount } from './state';
-import { useEffect } from 'react';
+import { useGlobalState, setCurrentAccount } from "./state";
+import { useEffect } from "react";
 
 export default function Home() {
-  const [currentAccount] = useGlobalState('currentAccount');
+  const [currentAccount] = useGlobalState("currentAccount");
 
   const checkIfWalletIsConnected = async () => {
     const { ethereum } = window;
@@ -22,19 +22,19 @@ export default function Home() {
       return;
     }
 
-    const accounts = await ethereum.request({ method: 'eth_accounts' });
+    const accounts = await ethereum.request({ method: "eth_accounts" });
 
     /*
-    * User can have multiple authorized accounts
-    */
+     * User can have multiple authorized accounts
+     */
     if (accounts.length !== 0) {
       const account = accounts[0];
       console.log("Found an authorized account:", account);
-      setCurrentAccount(account)
+      setCurrentAccount(account);
     } else {
-      console.log("No authorized account found")
+      console.log("No authorized account found");
     }
-  }
+  };
 
   const connectWallet = async () => {
     try {
@@ -45,17 +45,19 @@ export default function Home() {
         return;
       }
 
-      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
 
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   const renderNotConnectedContainer = () => (
-    <Image src={connectWalletButton} onClick={connectWallet}/>
+    <Image src={connectWalletButton} onClick={connectWallet} />
   );
 
   useEffect(() => {
@@ -63,22 +65,31 @@ export default function Home() {
   }, []);
 
   return (
-    <div class='flex flex items-center justify-center h-screen flex-col space-y-4'>
+    <div class="flex items-center justify-center h-screen flex-col">
+      <div className="mb-2">
+        <Image src={logo} />
+      </div>
+      <div className="mb-0">
+        <Image src={shpereText} />
+      </div>
+      <div className="mb-1">
+        <Image src={text} />
+      </div>
 
-      <Image src={logo} />
-      <Image src={shpereText} />
-      <Image src={text} />
-      
       {currentAccount === "" ? (
         renderNotConnectedContainer()
       ) : (
-        <form>
-          <input type="text" class="border-2 border-black rounded-sm" required />
-          <Link href="/menu">
-            <button>Register</button>
-          </Link>
+        <form className="mt-7 flex items-center gap-3">
+          <input
+            type="text"
+            className="border-2 border-black rounded-sm"
+            required
+          />
+          <div className="bg-black text-white px-4 py-1 rounded-lg">
+            <Link href="/menu">Register</Link>
+          </div>
         </form>
       )}
     </div>
-  )
+  );
 }
