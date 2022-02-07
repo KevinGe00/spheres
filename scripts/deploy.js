82339ea0286789548a476b12f85280a29716a653
Kevin Ge <kevingehd@gmail.com>
2022-02-06 16:17:13 -0800

    take out minting in deployment

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,11 +3,6 @@ const main = async () => {
     const nftContract = await nftContractFactory.deploy();
     await nftContract.deployed();
     console.log("Contract deployed to:", nftContract.address);
-
-    // Call the function.
-    let txn = await nftContract.createToken("BoredApeYachtClub")
-    // Wait for it to be mined.
-    await txn.wait()
 }
 
 const runMain = async () => {
