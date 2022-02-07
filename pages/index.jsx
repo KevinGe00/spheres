7a9858de7097d2acec92c218673652f1119118a8
Kevin Ge <kevingehd@gmail.com>
2022-02-06 16:23:20 -0800

    move game screen to sphere.js

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,40 @@
-import { ethers } from "ethers";
-import { useEffect, useState } from "react";
-import Link from "next/link";
-import axios from "axios";
-import styles from "../styles/Home.module.css";
-import Image from "next/image";
-import house from "./house.js";
+import { ethers } from 'ethers'
+import Link from 'next/link'
+import axios from 'axios'
+import styles from '../styles/Home.module.css'
+import Image from 'next/image'
+import logo from './Images/Landing/logo.png';
+import shpereText from './Images/Landing/Spheres.png';
+import text from './Images/Landing/Play. Connect. Explore..png';
+import connectWalletButton from './Images/Landing/connectWallet.png';
+
+import { useGlobalState, setCurrentAccount } from './state';
+import { useEffect } from 'react';
 
 export default function Home() {
-  const [currentAccount, setCurrentAccount] = useState("");
+  const [currentAccount] = useGlobalState('currentAccount');
+
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      alert("Make sure you have metamask!");
+      return;
+    }
+
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    /*
+    * User can have multiple authorized accounts
+    */
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account)
+    } else {
+      console.log("No authorized account found")
+    }
+  }
 
   const connectWallet = async () => {
     try {
@@ -18,136 +45,40 @@ export default function Home() {
         return;
       }
 
-      const accounts = await ethereum.request({
-        method: "eth_requestAccounts",
-      });
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
 
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
-      setupEventListener();
     } catch (error) {
       console.log(error);
     }
-  };
-
-  const [playerPosition, setPlayerPosition] = useState({ x: 20, y: 10 });
-
-  function downHandler({ key }) {
-    switch (key) {
-      case "w":
-        setPlayerPosition((pp) => ({ x: pp.x, y: pp.y - 1 }));
-        return;
-      case "s":
-        setPlayerPosition((pp) => ({ x: pp.x, y: pp.y + 1 }));
-        return;
-      case "a":
-        setPlayerPosition((pp) => ({ x: pp.x - 1, y: pp.y }));
-        return;
-      case "d":
-        setPlayerPosition((pp) => ({ x: pp.x + 1, y: pp.y }));
-        return;
-      default:
-        return;
-    }
   }
 
+  const renderNotConnectedContainer = () => (
+    <Image src={connectWalletButton} onClick={connectWallet}/>
+  );
+
   useEffect(() => {
-    window.addEventListener("keydown", downHandler);
-    return () => {
-      window.removeEventListener("keydown", downHandler);
-    };
+    checkIfWalletIsConnected();
   }, []);
 
   return (
-    <main className="fbg-green-500 h-screen">
-      <nav className="absolute left-0 h-screen z-10 bg-[#20253D] shadow-xl px-6 py-8">
-        <ul className="flex flex-col gap-7 h-full">
-          <li>
-            <button>
-              <Image src="/logo.svg" height={30} width={30} />
-            </button>
-          </li>
-          <li className="mt-auto">
-            <button
-              title="Build"
-              className="hover:scale-110 transition-transform"
-            >
-              <Image src="/hammer.svg" height={35} width={35} />
-            </button>
-          </li>
-          <li>
-            <button
-              title="Inventory"
-              className="hover:scale-110 transition-transform"
-            >
-              <Image src="/backpack.svg" height={35} width={35} />
-            </button>
-          </li>
-        </ul>
-      </nav>
-      <div
-        className="cursor-pointer grid h-full w-full overflow-auto"
-        style={{
-          gridTemplateColumns: "repeat(40, 50px)",
-          gridTemplateRows: "repeat(20, 50px)",
-        }}
-      >
-        {[...Array(20)].map((_, y) =>
-          [...Array(40)].map((_, x) => {
-            const tile = house.find((tile) => tile.x === x && tile.y === y);
+    <div class='flex flex items-center justify-center h-screen flex-col space-y-4'>
 
-            return (
-              <div className="relative" style={{ width: "55px" }}>
-                {tile ? (
-                  <div className="absolute">
-                    <Image src={tile.tile} height={55} width={55} />
-                  </div>
-                ) : (
-                  <div className="absolute">
-                    <Image src="/tiles/tile915.svg" height={55} width={55} />
-                  </div>
-                )}
-                {playerPosition.x === x && playerPosition.y === y && (
-                  <div className="absolute">
-                    <Image src="/avatar.svg" height={55} width={55} />
-                  </div>
-                )}
-              </div>
-            );
-          })
-        )}
-      </div>
-
-      <div className="m-3 mr-8 absolute right-0 top-0">
-        {currentAccount === "" ? (
-          <button
-            className="rounded-lg px-4 py-2 bg-[#20253D] text-white font-bold"
-            onClick={connectWallet}
-          >
-            Connect to Wallet
-          </button>
-        ) : (
+      <Image src={logo} />
+      <Image src={shpereText} />
+      <Image src={text} />
+      
+      {currentAccount === "" ? (
+        renderNotConnectedContainer()
+      ) : (
+        <form>
+          <input type="text" class="border-2 border-black rounded-sm" required />
           <Link href="/menu">
-            <button>Wallet connected, please click to continue.</button>
+            <button>Register</button>
           </Link>
-        )}
-      </div>
-
-      <div className="absolute flex items-center gap-4 bg-[#20253D] left-1/2 -translate-x-1/2 bottom-10 rounded-full px-7 py-3">
-        <button className="flex">
-          <Image src="/avatar.svg" className="m-auto" height={25} width={25} />
-        </button>
-        <p className="text-white min-w-[7rem] font-semibold">Bobvin</p>
-        <button className="flex">
-          <Image src="/train.svg" className="m-auto" height={20} width={20} />
-        </button>
-        <button className="flex">
-          <Image src="/mic.svg" className="m-auto" height={20} width={20} />
-        </button>
-        <button className="flex">
-          <Image src="/map.svg" className="m-auto" height={20} width={20} />
-        </button>
-      </div>
-    </main>
-  );
+        </form>
+      )}
+    </div>
+  )
 }
--- pages/sphere.js
+++ pages/sphere.js
@@ -1,13 +1,114 @@
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import house from "./house.js";
 import { useGlobalState } from './state';
-import Image from 'next/image'
-import SphereInGame from './Images/SphereInGame.png';
 
-export default function Sphere() {
-    const [currentCollection] = useGlobalState('currentCollection');
+export default function Home() {
+  
+  const [playerPosition, setPlayerPosition] = useState({ x: 20, y: 10 });
+  const [currentCollection] = useGlobalState('currentCollection');
 
-    return (
-        <div>
-            <Image src={SphereInGame} class="h-screen"/>
-        </div>
-    )
+  function downHandler({ key }) {
+    switch (key) {
+      case "w":
+        setPlayerPosition((pp) => ({ x: pp.x, y: pp.y - 1 }));
+        return;
+      case "s":
+        setPlayerPosition((pp) => ({ x: pp.x, y: pp.y + 1 }));
+        return;
+      case "a":
+        setPlayerPosition((pp) => ({ x: pp.x - 1, y: pp.y }));
+        return;
+      case "d":
+        setPlayerPosition((pp) => ({ x: pp.x + 1, y: pp.y }));
+        return;
+      default:
+        return;
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("keydown", downHandler);
+    return () => {
+      window.removeEventListener("keydown", downHandler);
+    };
+  }, []);
+
+  return (
+    <main className="fbg-green-500 h-screen">
+      <nav className="absolute left-0 h-screen z-10 bg-[#20253D] shadow-xl px-6 py-8">
+        <ul className="flex flex-col gap-7 h-full">
+          <li>
+            <button>
+              <Image src="/logo.svg" height={30} width={30} />
+            </button>
+          </li>
+          <li className="mt-auto">
+            <button
+              title="Build"
+              className="hover:scale-110 transition-transform"
+            >
+              <Image src="/hammer.svg" height={35} width={35} />
+            </button>
+          </li>
+          <li>
+            <button
+              title="Inventory"
+              className="hover:scale-110 transition-transform"
+            >
+              <Image src="/backpack.svg" height={35} width={35} />
+            </button>
+          </li>
+        </ul>
+      </nav>
+      <div
+        className="cursor-pointer grid h-full w-full overflow-auto"
+        style={{
+          gridTemplateColumns: "repeat(40, 50px)",
+          gridTemplateRows: "repeat(20, 50px)",
+        }}
+      >
+        {[...Array(20)].map((_, y) =>
+          [...Array(40)].map((_, x) => {
+            const tile = house.find((tile) => tile.x === x && tile.y === y);
+
+            return (
+              <div className="relative" style={{ width: "55px" }}>
+                {tile ? (
+                  <div className="absolute">
+                    <Image src={tile.tile} height={55} width={55} />
+                  </div>
+                ) : (
+                  <div className="absolute">
+                    <Image src="/tiles/tile915.svg" height={55} width={55} />
+                  </div>
+                )}
+                {playerPosition.x === x && playerPosition.y === y && (
+                  <div className="absolute">
+                    <Image src="/avatar.svg" height={55} width={55} />
+                  </div>
+                )}
+              </div>
+            );
+          })
+        )}
+      </div>
+
+      <div className="absolute flex items-center gap-4 bg-[#20253D] left-1/2 -translate-x-1/2 bottom-10 rounded-full px-7 py-3">
+        <button className="flex">
+          <Image src="/avatar.svg" className="m-auto" height={25} width={25} />
+        </button>
+        <p className="text-white min-w-[7rem] font-semibold">Bobvin</p>
+        <button className="flex">
+          <Image src="/train.svg" className="m-auto" height={20} width={20} />
+        </button>
+        <button className="flex">
+          <Image src="/mic.svg" className="m-auto" height={20} width={20} />
+        </button>
+        <button className="flex">
+          <Image src="/map.svg" className="m-auto" height={20} width={20} />
+        </button>
+      </div>
+    </main>
+  );
 }
