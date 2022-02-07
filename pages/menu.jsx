c20f21bc4888183a2c45279bd07f74d20d5c5ef7
Kevin Ge <kevingehd@gmail.com>
2022-02-06 17:18:14 -0800

    merge

--- pages/menu.jsx
+++ pages/menu.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 import { useGlobalState } from "./state";
 import { useNFTBalances } from "react-moralis";
 import Image from "next/image";
 import logo from "./Images/Landing/logo.png";
 import YourSphere from "./Images/SphereSelection/Your Spheres.png";
 import Link from "next/link";
+import NFT from '../utils/NFT.json';
+
 
 export default function Menu() {
   const [userNFTCollections, setUserNFTCollections] = useState([]);
@@ -13,6 +16,38 @@ export default function Menu() {
     useGlobalState("currentCollection");
   const { getNFTBalances, data, error, isLoading, isFetching } =
     useNFTBalances();
+    const CONTRACT_ADDRESS = "0xdcb5dD3BDa620AEd6641A6Fc90961BE78203Fb54";
+
+    const askContractToMintNft = async () => {
+      try {
+        const { ethereum } = window;
+    
+        if (ethereum) {
+          //const provider = new ethers.providers.Web3Provider(polygon);
+          const provider = new ethers.providers.JsonRpcProvider("https://rpc-mumbai.maticvigil.com")
+          const signer = provider.getSigner();
+          const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, NFT.abi, signer);
+    
+          console.log("Going to pop wallet now to pay gas...")
+          let nftTxn = await connectedContract.createToken(currentCollection);
+            
+          console.log("Mining...please wait.")
+          await nftTxn.wait();
+          
+          alert(`Minted :) see transaction: https://mumbai.polygonscan.com/tx/${nftTxn.hash}`);
+    
+        } else {
+          console.log("Ethereum object doesn't exist!");
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  
+    const handleSelect = (col) => {
+      setCurrentCollection(col)
+      askContractToMintNft()
+    }
 
   useEffect(() => {
     getNFTBalances({
@@ -52,7 +87,7 @@ export default function Menu() {
               <Link href="/sphere">
                 <button
                   className="border-2 border-black rounded-full px-3 py-1 font-bold"
-                  onClick={() => setCurrentCollection(collection)}
+                  onClick={() => handleSelect(collection)}
                 >
                   Join
                 </button>
